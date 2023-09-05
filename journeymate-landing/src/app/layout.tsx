efc6c4d44c049200109edd1913738b80a4b31f6d
marvinhuelsmann <mail@marvhuelsmann.com>
2023-09-05 23:08:17 +0200

    init commit

---
+++ journeymate-landing/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'JourneyMate - iOS App',
+  description: 'In JourneyMate kannst du deiner aktuellen Stadt eine persönliche Stimmung geben und diese mit einem JourneyFoto in deinem öffentlichen Profil verewigen.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
