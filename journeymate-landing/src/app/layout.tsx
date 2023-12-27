328c5ee91c40584217b6d79b89975b2e60c9f7a4
marvinhuelsmann <mail@marvhuelsmann.com>
2023-12-27 23:13:56 +0100

    update embed app link

--- journeymate-landing/src/app/layout.tsx
+++ journeymate-landing/src/app/layout.tsx
@@ -5,10 +5,6 @@ import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'JourneyMate - iOS App',
-  description: 'In JourneyMate kannst du deiner aktuellen Stadt eine persönliche Stimmung geben und diese mit einem JourneyFoto in deinem öffentlichen Profil verewigen.',
-}
 
 export default function RootLayout({
   children,
@@ -17,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <Head>
+      <meta name="apple-itunes-app" content="app-id=6448196185"/>
+    </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- journeymate-landing/src/app/page.tsx
+++ journeymate-landing/src/app/page.tsx
@@ -6,9 +6,6 @@ import Head from 'next/head';
 function Page() {
   return (
       <div>
-        <Head>
-          <meta name="apple-itunes-app" content="app-id=6448196185"/>
-        </Head>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <a href={"https://apps.apple.com/in/app/journeymate/id6448196185"} target={"_blank"} className="fixed left-0 top-0 flex w-full justify-center border-b  bg-gradient-to-b pb-6 pt-8 backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30">
