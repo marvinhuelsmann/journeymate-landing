bbda20d9d6dba9542ea3bcdbd77a1fb6ce42d416
marvinhuelsmann <mail@marvhuelsmann.com>
2023-12-27 23:32:40 +0100

    update embed app link

--- journeymate-landing/src/app/page.tsx
+++ journeymate-landing/src/app/page.tsx
@@ -18,7 +18,7 @@ function Page() {
                             <code className="font-mono font-bold">AppStore</code>
                         </a>
                         <div
-                            className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                            className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-black via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
                             <a
                                 className="pointer-events-none font-bold flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
                                 href="https://mhcreations.de"
@@ -39,7 +39,7 @@ function Page() {
                     </div>
 
                     <div
-                        className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+                        className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:blur-2xl after:content-[''] before:bg-gradient-to-br before:from-transparent before:to-blue-700 before:opacity-10 after:from-sky-900 after:via-[#0141ff] after:opacity-40 before:lg:h-[360px] z-[-1]">
                         <Image
                             className="relative drop-shadow-[0_0_0.3rem_#ffffff70] shadow-2xl"
                             src={AppIcon}
@@ -107,7 +107,7 @@ function Page() {
 
                         <a
                             href="mailto:support@mhcreations.de"
-                            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:bg-gray-100 hover:border-neutral-700 hover:bg-neutral-800/30"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
@@ -119,7 +119,7 @@ function Page() {
             </span>
                             </h2>
                             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                                Erhalte direkte Hilfe von anderen JourneyMate Nutzern oder unser Support-Team
+                                Erhalte direkte Hilfe von anderen JourneyMate Nutzern oder unserem Support-Team
                             </p>
                         </a>
                     </div>
