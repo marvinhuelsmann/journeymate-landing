1ea38fd644952fddc3e3dbac71cc9cdf7a108de0
marvinhuelsmann <mail@marvhuelsmann.com>
2023-12-27 23:00:33 +0100

    update embed app link

--- journeymate-landing/src/app/layout.tsx
+++ journeymate-landing/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+       <Head>
+           <meta name="apple-itunes-app" content="app-id=6448196185"/>
+       </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
