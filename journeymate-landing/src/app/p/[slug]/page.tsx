35b2054c22c28c377ebf0cab698684327978e454
marvinhuelsmann <mail@marvhuelsmann.com>
2024-01-04 02:03:10 +0100

    fix redirect

--- journeymate-landing/src/app/p/[slug]/page.tsx
+++ journeymate-landing/src/app/p/[slug]/page.tsx
@@ -1,22 +1,5 @@
-'use client'
-
-import {useRouter} from 'next/router';
-import {useEffect} from 'react';
+import {redirect} from "next/navigation";
 
 export default function Page({ params }: { params: { slug: string } }) {
-        const router = useRouter();
-
-        const redirectToApp = () => {
-                window.location.href = 'journeymate://journeymate.eu/post/' + params.slug;
-
-                const timeout = setTimeout(() => {
-                        window.location.href = 'https://apps.apple.com/us/app/journeymate/id6448196185'
-                }, 1000);
-
-                window.addEventListener('unload', () => clearTimeout(timeout));
-        };
-
-        useEffect(() => {
-                redirectToApp();
-        }, []);
+        redirect('journeymate://journeymate.eu/post/' + params.slug)
 }
